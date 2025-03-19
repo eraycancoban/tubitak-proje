42792443e6127dcafc8ffcdcd3e99eb118ae9e8a
Eraycan Coban <103067410+eraycancoban@users.noreply.github.com>
2025-03-19 11:15:04 +0300

    Delete finalApi directory

--- finalApi/routes/chat.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import  {db} from "../db.js";
-import { getChatReply,addChatLog} from "../controllers/chat.js";
- 
-const router = express.Router();
-router.post("/getChatReply", getChatReply);
-router.post("/addChatLog", addChatLog);
-export default router;
